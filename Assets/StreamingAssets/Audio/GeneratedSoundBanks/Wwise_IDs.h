f0c92e2622e329ef33ffbdc1794570bf1ae5e038
Ali Cedroni <AliCedroni66@gmail.com>
2018-09-27 21:24:07 +0200

    more wwise stuff

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,25 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID ANETTA = 4221601770U;
+        static const AkUniqueID CHOICEAPPEAR = 212160671U;
         static const AkUniqueID GENERAL = 133642231U;
+        static const AkUniqueID GROUP = 3944382562U;
+        static const AkUniqueID HEMA = 3649605926U;
+        static const AkUniqueID HOME = 3181406844U;
+        static const AkUniqueID ISTYPING = 2045542996U;
+        static const AkUniqueID JESSIE = 2992761690U;
+        static const AkUniqueID JIMMY = 2836931289U;
+        static const AkUniqueID KALIL = 2229701220U;
+        static const AkUniqueID KEYBOARD = 1403540682U;
+        static const AkUniqueID KYLE = 2565061260U;
+        static const AkUniqueID MESSAGE = 3690986026U;
+        static const AkUniqueID MORGAN = 3330463761U;
+        static const AkUniqueID PICTURE = 945962381U;
+        static const AkUniqueID REPLY = 3866603715U;
+        static const AkUniqueID SEVENDAYS = 37891289U;
+        static const AkUniqueID SUBMIT = 2320974141U;
+        static const AkUniqueID TANYA = 1731911000U;
     } // namespace EVENTS
 
     namespace BANKS
